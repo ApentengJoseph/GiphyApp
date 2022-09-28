07611470130981376efc2e3df720b3ad6efa2233
Apenteng Joseph <apentengjoseph59@gmail.com>
2022-09-28 23:39:14 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ResultListComponent } from './result-list/result-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import { GifdetailsComponent } from './gifdetails/gifdetails.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbarComponent,
+    ResultListComponent,
+    GifdetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
